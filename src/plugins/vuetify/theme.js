06c593cad38782a79b806f385eab5dcad6adca36
root <root@pokemons.asia-east1-b.c.halogen-framing-401400.internal>
2023-11-20 11:11:18 +0000

    fix: 變更網頁主題色&圖片變更

--- src/plugins/vuetify/theme.js
+++ src/plugins/vuetify/theme.js
@@ -1,7 +1,7 @@
 import { resolveVuetifyTheme } from '@core/utils/vuetify'
 import { themeConfig } from '@themeConfig'
 
-export const staticPrimaryColor = '#7367F0'
+export const staticPrimaryColor = '#ADD7F0'
 
 const theme = {
   defaultTheme: resolveVuetifyTheme(),
