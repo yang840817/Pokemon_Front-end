ef231ea1aed52b33c474f317d9427eeb6a332cb2
yang840817 <yang840817@gmail.com>
2023-11-29 06:43:20 +0000

    feat: 優化商店選購頁面
    1. 商品加入購物車時新增snackbar提示
    2. 切換商品頁時新增route.query pages記錄當前頁面資料，
       優化使用者體驗，重整時會再對應頁面

--- src/pages/shop.vue
+++ src/pages/shop.vue
@@ -1,18 +1,56 @@
 <script setup>
-import { useCartStore } from '@/stores/cart'
-import { useRaceStore } from '@/stores/pokemon'
+import { onBeforeMount, watchEffect } from "vue";
+import { useCartStore } from "@/stores/cart";
+import { useRaceStore } from "@/stores/pokemon";
+import { useSnackbarStore } from "@/stores/snackbar";
+import { useRouter, useRoute } from "vue-router";
 
+const cartStore = useCartStore();
+const raceStore = useRaceStore();
+const snackbarStore = useSnackbarStore();
+const router = useRouter();
+const route = useRoute();
 
-const raceStore = useRaceStore()
-const cartStore = useCartStore()
-
+onBeforeMount(async () => {
+  try {
+    const { page } = route.query;
+    if (page) {
+      raceStore.fetchRaceList(page);
+    } else {
+      raceStore.fetchRaceList();
+    }
+  } catch (error) {}
+});
+watchEffect(() => {
+  const { page } = router.currentRoute.value.query;
+  if (page) {
+    raceStore.fetchRaceList(page);
+  }
+});
 async function changPage(e) {
-  raceStore.fetchRaceList(e)
+  raceStore.fetchRaceList(e);
+  router.push({ query: { page: e } });
 }
 
-raceStore.fetchRaceList()
-
-const pokemons = computed(() => { return raceStore.raceList.data })
+const pokemons = computed(() => {
+  return raceStore.raceList.data;
+});
+async function onAddItem(id) {
+  try {
+    await cartStore.addItem(id, 1);
+    snackbarStore.message = "Added Successfully";
+    snackbarStore.isShow = true;
+    snackbarStore.timeout = 1500;
+    snackbarStore.noticeType(true);
+  } catch (error) {
+    console.log(error);
+    if (error.response.status === 401) return router.push("/login");
+    snackbarStore.message = error.response.data.message;
+    snackbarStore.isShow = true;
+    snackbarStore.timeout = 10000;
+    snackbarStore.noticeType(false);
+  }
+}
 </script>
 
 <template>
@@ -28,15 +66,16 @@ const pokemons = computed(() => { return raceStore.raceList.data })
         >
           <VCard>
             <VToolbar color="rgba(100, 0, 0, 0)">
-              <VToolbarTitle class="text-h6 pokemon-name ">
+              <VToolbarTitle class="text-h6 pokemon-name">
                 {{ pokemon.name }}
               </VToolbarTitle>
               <VBtn
+                v-if="pokemon.stock"
                 icon="mdi-cart-plus"
-                @click="cartStore.addItem(({
-                  race_id:pokemon.id,quantity:1
-                }))"
+                color="#00F3FF"
+                @click="onAddItem(pokemon.id)"
               />
+              <VCardText v-else class="flex-grow-0">Sold Out</VCardText>
             </VToolbar>
 
             <VImg
@@ -48,7 +87,9 @@ const pokemons = computed(() => { return raceStore.raceList.data })
         </VCol>
       </VRow>
     </section>
-    <section class="d-flex align-center justify-sm-space-between justify-center flex-wrap gap-3 pt-3">
+    <section
+      class="d-flex align-center justify-sm-space-between justify-center flex-wrap gap-3 pt-3"
+    >
       <VDivider />
       <p class="text-sm text-disabled mb-0">
         total:{{ raceStore.raceList.total }}
@@ -66,7 +107,7 @@ const pokemons = computed(() => { return raceStore.raceList.data })
   </div>
 </template>
 
-<style  lang="scss" scoped>
+<style lang="scss" scoped>
 :deep(.v-toolbar-title__placeholder) {
   text-transform: capitalize;
 }
